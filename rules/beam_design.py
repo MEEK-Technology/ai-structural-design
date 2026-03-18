36597d0a4e102df8a24a3c67705eb371c3939d30
Mightysho <mightysho4sure2014@gmail.com>
2026-03-18 16:39:20 +0100

    commit: beam design dataset

---
+++ rules/beam_design.py
@@ -0,0 +1,34 @@
+# rules/beam_design.py
+
+def bending_moment(load, span):
+    """
+    Calculate bending moment (kNm)
+    load: kN/m
+    span: m
+    """
+    return (load * span**2) / 8
+
+
+def steel_area(Mu, fy, d):
+    """
+    Calculate steel area (mm²)
+    Mu: bending moment (kNm)
+    fy: yield strength (MPa)
+    d: effective depth (mm)
+    """
+    Mu_Nmm = Mu * 1e6  # convert kNm to Nmm
+    z = 0.9 * d
+    return Mu_Nmm / (0.87 * fy * z)
+
+
+def design_beam(load, span, fy=500, d=450):
+    """
+    Main function
+    """
+    Mu = bending_moment(load, span)
+    As = steel_area(Mu, fy, d)
+
+    return {
+        "bending_moment": round(Mu, 2),
+        "steel_area": round(As, 2)
+    }
