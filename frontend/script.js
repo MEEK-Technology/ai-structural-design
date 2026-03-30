0553ad5b5be509dbdb892c25fbdf2caac1b8e24d
Mightysho <mightysho4sure2014@gmail.com>
2026-03-30 22:30:19 +0100

    added frontend javascript to project

---
+++ frontend/script.js
@@ -0,0 +1,16 @@
+async function generate() {
+    const prompt = document.getElementById("prompt").value;
+
+    const response = await fetch("http://127.0.0.1:8000/predict", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ prompt: prompt })
+    });
+
+    const data = await response.json();
+
+    document.getElementById("steel").innerText = data.results.steel_area;
+    document.getElementById("moment").innerText = data.results.bending_moment;
+}
