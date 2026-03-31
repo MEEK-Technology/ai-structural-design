ab43acfde8a9fd72b2c4da850c60d6055aae4f0e
Mightysho <mightysho4sure2014@gmail.com>
2026-03-31 04:51:55 +0100

    added updated javascript to project

--- frontend/script.js
+++ frontend/script.js
@@ -1,7 +1,27 @@
+// async function generate() {
+//     const prompt = document.getElementById("prompt").value;
+
+//     const response = await fetch("http://127.0.0.1:8000/predict", {        
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json"
+//         },
+//         body: JSON.stringify({ prompt: prompt })
+//     });
+
+//     const data = await response.json();
+
+//     document.getElementById("steel").innerText = data.results.steel_area;
+//     document.getElementById("moment").innerText = data.results.bending_moment;
+// }
+
+
+let shearChart, momentChart;
+
 async function generate() {
     const prompt = document.getElementById("prompt").value;
 
-    const response = await fetch("http://127.0.0.1:8000/predict", {        
+    const response = await fetch("http://127.0.0.1:8000/predict", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
@@ -13,4 +33,38 @@ async function generate() {
 
     document.getElementById("steel").innerText = data.results.steel_area;
     document.getElementById("moment").innerText = data.results.bending_moment;
+
+    drawCharts(data.graphs);
+}
+
+function drawCharts(graphs) {
+    const ctx1 = document.getElementById("shearChart").getContext("2d");
+    const ctx2 = document.getElementById("momentChart").getContext("2d");
+
+    if (shearChart) shearChart.destroy();
+    if (momentChart) momentChart.destroy();
+
+    shearChart = new Chart(ctx1, {
+        type: "line",
+        data: {
+            labels: graphs.x,
+            datasets: [{
+                label: "Shear Force",
+                data: graphs.shear,
+                fill: false
+            }]
+        }
+    });
+
+    momentChart = new Chart(ctx2, {
+        type: "line",
+        data: {
+            labels: graphs.x,
+            datasets: [{
+                label: "Bending Moment",
+                data: graphs.moment,
+                fill: false
+            }]
+        }
+    });
 }
